32ff97f5d4b574977827170ad67f57608aefc9eb
jonny64 <astro64m@gmail.com>
2016-02-23 17:09:15 +0000

    btcjam: use library login

--- btcjam.js
+++ btcjam.js
@@ -1,6 +1,10 @@
-var casper = init_casper ();
+var common = require('./common.js');
 
-login (casper);
+var casper = common.init_casper ();
+
+casper.then(function notes_login(){
+	common.login (casper, casper.config.user, casper.config.password);
+});
 
 casper.thenOpen(jam_datatables_notes_url (0, 10), jam_datatables_headers(), function notes_page_ping(response){
 
@@ -10,7 +14,7 @@ casper.thenOpen(jam_datatables_notes_url (0, 10), jam_datatables_headers(), func
 
 	if (is_empty && is_send_empty_notify ()) {
 
-		pushbullet({
+		common.pushbullet({
 			body  : this.getPageContent(),
 			title : 'parsing is possibly broken'
 		}, casper);
@@ -42,7 +46,7 @@ casper.then(function notify_found_notes(){
 
 	require('utils').dump(all_notes);
 
-	write_json(all_notes, 'notes');
+	common.write_json(all_notes, 'notes');
 
 	notify_notes(all_notes, this);
 });
@@ -138,7 +142,7 @@ function notify_notes(page_notes, casper){
 		body = body + '\n\n';
 	}
 
-	pushbullet({
+	common.pushbullet({
 		body  : body,
 		title : page_notes.length + ' new notes found ' + subject_postfix
 	}, casper);
@@ -148,7 +152,7 @@ function parse_notes(raw_page_notes, casper) {
 
 	var page_notes = [];
 
-	var skip_notes = casper.config.skip.notes.concat(ids(load_json('notes')));
+	var skip_notes = casper.config.skip.notes.concat(common.ids(common.load_json('notes')));
 
 	for (var i in raw_page_notes) {
 
@@ -313,152 +317,4 @@ function write_run_flag(name){
 	var fs = require('fs');
 
 	fs.write(name + '.json', JSON.stringify({dt: new Date()}), 'w');
-}
-
-
-function ids(records) {
-
-	var ids = []
-	for (var i in records) {
-		ids.push(records[i].id || records[i].id_listing);
-	}
-	return ids;
-}
-
-function write_json(add_records, name){
-
-	var fs = require('fs');
-
-	var records = load_json(name);
-
-	for (var i in add_records) {
-		records.push(add_records[i]);
-	}
-
-	fs.write(name + '.json', JSON.stringify(records || []), 'w');
-}
-
-function load_json(name){
-
-	var file = name + '.json';
-
-	var fs = require('fs');
-
-	if (!fs.isFile(file)) {
-		return [];
-	}
-
-	var json = fs.read(file) || [];
-
-	return JSON.parse(json) || [];
-}
-
-function init_casper() {
-
-	var fs = require('fs');
-	var config_file = fs.read('btcjam.json');
-	var config = JSON.parse(config_file) || {};
-
-	var casper = require('casper').create({
-		timeout: 240000,
-		waitTimeout: 60000,
-		verbose: true,
-		logLevel: config.debug? 'debug' : 'info',
-		pageSettings: {
-			userAgent: 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
-			sslPprotocol: "tlsv1"
-		}
-	});
-
-	casper.config = config;
-	casper.config.skip.notes = casper.config.skip.notes || [];
-	casper.config.skip.borrowers = casper.config.skip.borrowers || [];
-
-	casper.on('remote.message', function(msg) {
-		this.log('remote message caught: ' + msg, 'info');
-	});
-
-	casper.on("page.error", function(msg, trace) {
-		this.captureSelector('error.png', 'html');
-		this.log("Page Error: " + msg, "warning");
-		for(var i=0; i<trace.length; i++) {
-			var step = trace[i];
-			this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
-		}
-	});
-
-	casper.on("error", function(msg, trace) {
-		this.captureSelector('error.png', 'html');
-		this.log(msg, "error");
-		// pushbullet ({'body' : '[aws][btcjam] page error'}, this);
-	});
-
-	casper.on("wait.timeout", function(msg, trace) {
-		this.captureSelector('error.png', 'html');
-		this.log(msg, "waitTimeout");
-		// pushbullet ({'body' : '[aws][btcjam] page error'}, this);
-	});
-
-	casper.on('resource.error',function (request) {
-	    this.log(request.url + ' ' + request.errorString, 'warning');
-	});
-
-	return casper;
-}
-
-function login(casper) {
-
-	casper.start(casper.config.url, function login() {
-
-		casper.waitForSelector('#user_email.email', function fill_login_form() {
-			if (this.config.debug) {
-				this.captureSelector('before_login.png', 'html');
-			}
-			this.fill('form#new_user', { 'user[email]': casper.config.user, 'user[password]': casper.config.password }, true);
-		});
-	}, function error_popup(){
-		this.captureSelector('error_login.png', 'html');
-	}, 15000)
-	.waitForText('Dashboard');
-
-	if (casper.config.debug) {
-		casper.then(function after_submit(){
-			this.captureSelector('logged_on.png', 'html');
-		});
-	}
-}
-
-function pushbullet(options, casper) {
-
-	var TARGET_EMAIL = casper.config.pushbullet.email;
-
-	if (TARGET_EMAIL) {
-		options["email"] = TARGET_EMAIL;
-		return 0;
-	}
-
-	var API_KEY = casper.config.pushbullet.api_key;
-
-	if (!API_KEY) {
-		casper.log("casper.config.pushbullet.api_key missing", "error");
-		return 0;
-	}
-
-	options["type"]  = options.type || "note";
-	options["url"]   = options.url || "";
-	options["title"] = options.title || "";
-	options["body"]  = options.body || "";
-
-	casper.open("https://api.pushbullet.com/v2/pushes", {
-		method: 'post',
-		data:   JSON.stringify (options),
-		headers: {
-			'Content-type': 'application/json',
-			'Accept': 'application/json',
-			"Authorization": "Bearer " + API_KEY
-		}
-	}, function(response){
-		require('utils').dump(this.page.content);
-	});
-
 }
